b192eed9fac5b7da9fc1ffc709e31d69a99040d1
Evgeniy Olekhnovich <jeniaole13@mail.ru>
2016-03-15 11:27:23 +0300

    Added files via upload

---
+++ probit.R
@@ -0,0 +1,31 @@
+probit.analysis <- function(x){
+  library(drc)
+  fitl.glm <- glm(cbind(x[,3], x[,2]-x[,3]) ~ log(x[,1]),
+                  family=binomial('probit'), data=x[x$dose != 0, ])
+  e <- summary(fitl.glm)
+  fit <- anova(fitl.glm, test = 'LRT')
+  dose.p
+  xp <- dose.p(fitl.glm, p=c(0.50, 0.95, 0.99))
+  xp.ci <- xp + attr(xp, "SE") %*% matrix(qnorm(1 - 0.05/2)*c(-1,1), nrow=1)
+  zp.est <- exp(cbind(xp.ci[,1],xp,xp.ci[,2]))
+  dimnames(zp.est)[[2]] <- c("zp.lcl","zp","zp.ucl")
+  a <- fitl.glm$fitted.values
+  b <- x[,3]/x[,2]
+  d <- x[,1]
+  c <- data.frame(a,b,d)
+  library(ggplot2)
+  plot <- ggplot(c, aes(x = log(d), y = log(b)))+
+    geom_point(size = 3)+
+    geom_smooth(method = "lm", aes(x = log(d), y = log(a)))+
+    theme_linedraw()
+  chisq <- fit$`Resid. Dev`[2]
+  DF <- fit$`Resid. Df`[2]
+  chisq.df <- data.frame(chisq, DF)
+  slope <- e$coefficients[2,1]
+  SE <- e$coefficients[2,2]
+  slope.SE <- data.frame(slope, SE)
+  print(zp.est)
+  print(slope.SE)
+  print(chisq.df)
+  plot
+}
